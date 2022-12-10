669134789fa9789e0c68562875fc052ab442d4c8
Amanda Stern <amanda.stern.73@gmail.com>
2022-12-10 15:29:15 -0500

    stub up form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
 import './App.css';
 
 function App() {
@@ -19,7 +23,39 @@ function App() {
 
 
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Box>
+        <Paper>
+          <form>
+            <TextField
+              type='text'
+              name='firstName'
+              label='First Name'
+              value={formData.firstName}
+            />
+            <TextField
+              type='text'
+              name='lastName'
+              label='Last Name'
+              value={formData.lastName}
+            />
+            <TextField
+              type='text'
+              name='email'
+              label='Email'
+              value={formData.email}
+            />
+            <TextField
+              type='text'
+              name='location'
+              label='Location'
+              value={formData.location}
+            />
+            <Button type='submit'>Submit</Button>
+          </form>
+        </Paper>
+      </Box>
+    </div>
   );
 }
 
